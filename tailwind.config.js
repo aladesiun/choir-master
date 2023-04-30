3bf410ccaf651ea55ecdcce3ba0b7e8fcc9bfbc8
pelumi <126729110+Pelwebdev@users.noreply.github.com>
2023-04-30 20:19:33 +0100

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  darkMode: 'media',
+  // darkMode: 'media',
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
     "./node_modules/flowbite/**/*.js",
