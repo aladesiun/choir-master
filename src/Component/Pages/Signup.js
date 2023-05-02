47f6ecc525b191a56cd9259589a13923e09ccfb0
pelumi <126729110+Pelwebdev@users.noreply.github.com>
2023-05-02 11:51:10 +0100

    updated

--- src/Component/Pages/Signup.js
+++ src/Component/Pages/Signup.js
@@ -22,11 +22,11 @@ const Signup = () => {
                 <div className='flex space-x-5'>
                   <div>
                     <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First name</label>
-                    <input value={input.name} onChange={handleInput} type="name" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="your first name" required="" />
+                    <input value={input.name} onChange={handleInput} type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="your first name" required="" />
                   </div>
                   <div>
                     <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last name</label>
-                    <input value={input.lastName} onChange={handleInput} type="name" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="your last name" required="" />
+                    <input value={input.lastName} onChange={handleInput} type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="your last name" required="" />
                   </div>
                 </div>
                 <div>
