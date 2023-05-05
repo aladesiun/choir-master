9bd4435e14c4d1d21490d61f523e43adc75c8cd6
temitope <aladesiuntope@gmail.com>
2023-05-05 10:33:58 +0100

    added env properly

--- server/database.js
+++ server/database.js
@@ -1,12 +1,12 @@
 const { createConnection } = require("mysql");
+require('dotenv').config();
 
 const db = createConnection({
-    host:"127.0.0.1",
-    port:"8889",
-    database:"choir_master",
-    user:'root',
-    password:'root',
-    connectionLimit:10
+    host:process.env.DB_HOST,
+    port:process.env.DB_PORT,
+    database:process.env.DB_NAME,
+    user:process.env.DB_USER,
+    password:process.env.PASSOWRD,
 })
 
 
