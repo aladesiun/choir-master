a735be03b00832ab17e6d6e1dd5fe9e365a53586
Kennypapa <daramolakehinde339@gmail.com>
2023-05-08 21:35:21 +0100

    create-new-song

--- src/App.js
+++ src/App.js
@@ -8,13 +8,12 @@ import Signup from './Component/Pages/Signup';
 import Login from './Component/Pages/Login';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Footer from './Component/Footer';
-import HomePagesSec from './Component/Pages/HomePagesSec';
-import { SkeletonTheme } from 'react-loading-skeleton';
+import Song from './Component/Pages/Song';
 import NewSong from './Component/Pages/NewSong';
 function App() {
   return (
     <div className="App">
-      <SkeletonTheme baseColor='#be3bd1' highlightColor='#be3bd1'>
+     
       <BrowserRouter>
       <Header />
         <Routes>
@@ -24,12 +23,12 @@ function App() {
             <Route path='/Code' element={<Code />}/>
             <Route path='/Signup' element={<Signup />}/>
             <Route path='/Login' element={<Login />}/>
-            <Route path='/song/:id' element={<HomePagesSec />}/>
+            <Route path='/Song/:id' element={<Song />}/>
             <Route path='/song/create' element={<NewSong />}/>
         </Routes>
         <Footer />
       </BrowserRouter>
-      </SkeletonTheme>
+     
     </div>
   );
 }
