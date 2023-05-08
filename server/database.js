9dc7e84a3b94b2f796d749cc537a3487b212b720
temitope <aladesiuntope@gmail.com>
2023-05-08 22:18:25 +0100

    updates

--- server/database.js
+++ server/database.js
@@ -6,8 +6,9 @@ const db = createConnection({
     port:process.env.DB_PORT,
     database:process.env.DB_NAME,
     user:process.env.DB_USER,
-    // password:process.env.PASSOWRD,
+    password:process.env.PASSOWRD,
 })
 
 
 module.exports = db;
+
