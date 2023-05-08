850dc10bce87fdd7d21e6c95263b80a9921d28cd
Kennypapa <daramolakehinde339@gmail.com>
2023-05-08 21:41:16 +0100

    tinymce

--- src/Component/Pages/NewSong.js
+++ src/Component/Pages/NewSong.js
@@ -21,11 +21,11 @@ const NewSong = () => {
     }
   return (
     <div className="my-5">
-      <section className="bg-gray-50 dark:bg-gray-900 pt-20 md:pt-0">
-        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <section className="bg-gray-50 dark:bg-gray-900 pt-32 pb-20">
+        <div className="flex flex-col items-center justify-center px-6 mx-auto md:h-screen lg:py-0">
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-lg xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              <h1 className="text-xl font-bold leading-tight uppercase tracking-tight text-gray-900 md:text-2xl dark:text-white">
                 Create  Your Song!
               </h1>
               <form
