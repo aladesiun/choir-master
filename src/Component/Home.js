76b08cb2db49a60f51362d5f9443de589ec4cfec
temitope <aladesiuntope@gmail.com>
2023-05-09 15:36:06 +0100

    updats

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -4,7 +4,17 @@ import axios from "axios";
 const HomePage = () => {
   const [allSongs, setAllSongs] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [searchString, setSearchString] = useState("");
+  const [fillterString, setFillterString] = useState("");
   
+  const query = ()=>{
+
+  }
+
+  const handleInput = (e) => {
+    setSearchString(e.target.value)
+    console.log(searchString);
+  }
   useEffect(() => {
     setIsLoading(true);
     axios
@@ -22,6 +32,7 @@ const HomePage = () => {
   }, []);
   return (
     <section className="bg-gray-50 dark:bg-gray-900 py-32 px-3 lg:px-6">
+      
       <div>
         {isLoading ? (
           <ul
@@ -50,7 +61,22 @@ const HomePage = () => {
             </li>
           </ul>
         ) : (
-          <ul className="container max-w-5xl m-auto border bg-white dark:bg-gray-800 rounded-lg border-gray-100 dark:border-gray-700 px-3 p-6 lg:px-8 grid grid-cols-1 md:grid-cols-2 gap-4 lg:gap-8">
+          <div className="container max-w-5xl m-auto">
+          <div className='my-5 flex'>
+              <select name='search' value={fillterString} onChange={(e)=> setFillterString(e.target.value)} className="p-2 max-w-[125px] text-sm text-gray-900 border rounded-r-none border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <option  className='text-gray-600 dark:text-gray-300 font-black' value=' '>By:</option>
+                <option value="title">Title</option>
+                <option value="username">Username</option>
+              </select>
+              <div className="relative hidden md:block">
+                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                  <svg className="w-5 h-5 text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
+                  <span className="sr-only">Search icon</span>
+                </div>
+                <input value={searchString} onChange={handleInput} placeholder={"by "+" " + fillterString} type="text" id="search-navbar" className="block w-full p-2 pl-10 text-sm text-gray-900 border rounded-l-none border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+              </div>
+            </div>
+          <ul className=" border bg-white dark:bg-gray-800 rounded-lg border-gray-100 dark:border-gray-700 px-3 p-6 lg:px-8 grid grid-cols-1 md:grid-cols-2 gap-4 lg:gap-8">
             {allSongs.map((song, index) => (
               <li key={index}>
                 <Link to={`/song/${song.id}`}>
@@ -78,6 +104,7 @@ const HomePage = () => {
               </li>
             ))}
           </ul>
+          </div>
         )}
       </div>
     </section>
