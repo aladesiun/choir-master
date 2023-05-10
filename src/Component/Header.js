6b92848328b4d328dd97d264311aafeb5d131a57
Kennypapa <daramolakehinde339@gmail.com>
2023-05-10 16:12:52 +0100

    logout

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,15 +1,21 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Header = () => {
-
+  
   const [searchString, setSearchString] = useState("");
   const [fillterString, setFillterString] = useState("");
   const user = JSON.parse(localStorage.getItem('user') ?? "[]");
+  const navigate = useNavigate();
 
   const handleInput = (e) => {
     setSearchString(e.target.value)
     console.log(searchString);
+  }
+
+  const logOut = () => {
+    localStorage.clear();
+    navigate('/');
   }
 
   return (
@@ -65,7 +71,7 @@ const Header = () => {
                 <Link to='/' className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Home</Link>
               </li>
               <li>
-                <Link to='/login' className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">{user.token ? 'Logout' : 'Login'} </Link>
+                <Link to='/login' onClick={logOut} className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">{user.token ? 'Logout' : 'Login'} </Link>
               </li>
               <li>
                 <Link to='/song/create' className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Create song</Link>
